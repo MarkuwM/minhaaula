ba5195c52a3edf4a11fe3e1669b3636bed520e9a
MarkuwM <silvaalderlindson@gmail.com>
2025-01-14 20:40:37 -0300

    lista

---
+++ lista/inss.js
@@ -0,0 +1,15 @@
+function previdencia(salario){
+    if(salario <= 1412){
+        return(salario * 0.075)
+    }else if(salario <= 2666.68){
+        return(salario * 0.09)
+    }else if(salario <= 4000.03){
+        return(salario * 0.12)
+    }else if(salario <= 7786.02){
+        return(salario * 0.14) - 181.18
+    }else{
+        return(7786.02 * 0.14) - 181.18
+    }
+}
+var imposto = previdencia(3000)
+document.write('<h1>Seu INSS é ${imposto}</h1>')
