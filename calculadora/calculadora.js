52cea0f718e38904e990028b84ff9f83eab50cf9
MarkuwM <silvaalderlindson@gmail.com>
2025-01-28 19:38:33 -0300

    calculadora funcionando

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,38 +1,42 @@
-function adicao(a, b){
+function adicao(a, b) {
     return a + b
 }
-function subtracao(a, b){
+function subtracao(a, b) {
     return a - b
 }
-function multiplicacao(a, b){
+function multiplicacao(a, b) {
     return a * b
 }
-function divisao(a, b){
-    if(b == 0){
+function divisao(a, b) {
+    if (b == 0) {
         return null
     }
     return a / b
 }
-function calcular(){
-    var n1 = document.getElementById('n1').value
-    var n2 = document.getElementById('n2').value
-    var op = document.getElementsByName('op')
-    var resultado = document.getElementById('resultado').value
-    if(op[0].checked){
-        var s = adicao(n1, n2)
+
+var n1 = document.getElementById('n1')
+var n2 = document.getElementById('n2')
+var op = document.getElementsByName('op')
+var resultado = document.getElementById('resultado')
+
+function calcular() {
+let m1 = parseFloat(n1.value)
+let m2 = parseFloat(n2.value)
+    if (op[0].checked) {
+        var s = adicao(m1, m2)
         resultado.innerText = s
-    } else if(op[1].checked){
-        var s = subtracao(n1, n2)
+    } else if (op[1].checked) {
+        var s = subtracao(m1, m2)
         resultado.innerText = s
-    } else if(op[2].checked){
-        var s = multiplicacao(n1, n2)
+    } else if (op[2].checked) {
+        var s = multiplicacao(m1, m2)
         resultado.innerText = s
-    } else if(op[3].checked){
-        var s = divisao(n1, n2)
-        if(s == null){
+    } else if (op[3].checked) {
+        var s = divisao(m1, m2)
+        if (s == null) {
             alert('ERRO: DIVISÃO POR ZERO')
         }
     } else {
         resultado.innerText = s
     }
-}    
+}
